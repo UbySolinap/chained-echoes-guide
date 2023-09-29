61ac6b879cf040e9759e7c8436830b3c3d17bd4c
UbySolinap <ubys06@gmail.com>
2023-09-29 20:38:28 +0800

    Updated pictures path v2

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import styles from "./Logo.module.css";
 function Logo() {
   return (
     <Link to="/">
-      <img src="./public/logo.png" alt="Guide Logo" className={styles.logo} />
+      <img src="logo.png" alt="Guide Logo" className={styles.logo} />
     </Link>
   );
 }
--- src/pages/Basics.jsx
+++ src/pages/Basics.jsx
@@ -1,7 +1,15 @@
+import PageHeader from "../components/PageHeader";
+import PageNav from "../components/PageNav";
+
 import styles from "./Basics.module.css";
 
 function Basics() {
-  return <div>Basics</div>;
+  return (
+    <main>
+      <PageNav />
+      <PageHeader img={"gamebasics"}>Game Basics</PageHeader>
+    </main>
+  );
 }
 
 export default Basics;
